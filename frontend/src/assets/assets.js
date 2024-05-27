16e200cb01875cc20ce60967c628fd8cba0fe51a
Sowbaranika1111 <sowbika002@gmail.com>
2024-05-27 16:32:34 +0530

    Updated Home page images coding

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,13 +1,57 @@
 //Importing images and icons
 import boy_happy_img from './boy_happy_img.png'
 import search_icon from './search_icon.png'
+import exp_1_img from './exp_1_mental_health_img.jpg'
+import exp_2_img from './exp_2_physical_health_img.jpg'
+import exp_3_img from './exp_3_manifestations_img.webp'
+import exp_4_img from './exp_4_miracles_img.webp'
+import exp_5_img from './exp_5_healing_img.png'
+import exp_6_img from './exp_6_visions_img.jpg'
+import exp_7_img from './exp_7_msgs_received_img.png'
+import exp_8_img from './exp_8_astral_travel_img.jpg'
+import exp_9_img from './exp_9_other_exp_img.png'
 
 //exported it in objects format , objects name is assets
 export const assets = {
     boy_happy_img,
-    search_icon
+    search_icon,
 }
 
-export const experiences_list = {
-    
-}
+export const experiences_list = [
+    {
+        exp_name: "Mental_Health",
+        exp_img: exp_1_img
+    },
+    {
+        exp_name: "Physical_Health",
+        exp_img: exp_2_img
+    },
+    {
+        exp_name: "Manifestations",
+        exp_img: exp_3_img
+    },
+    {
+        exp_name: "Miracles",
+        exp_img: exp_4_img
+    },
+    {
+        exp_name: "Healing",
+        exp_img: exp_5_img
+    },
+    {
+        exp_name: "Visions",
+        exp_img: exp_6_img
+    },
+    {
+        exp_name: "Messages_received",
+        exp_img: exp_7_img
+    },
+    {
+        exp_name: "Astral_Travel",
+        exp_img: exp_8_img
+    },
+    {
+        exp_name: "Other_Experiences",
+        exp_img: exp_9_img
+    },
+]
--- frontend/src/components/ExploreExperiences/ExploreExperiences.jsx
+++ frontend/src/components/ExploreExperiences/ExploreExperiences.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './ExploreExperiences.css'
+import { experiences_list } from '../../assets/assets'
 
 const ExploreExperiences = () => {
   return (
-    <div>
-      
+    <div className='explore-exp' id='explore-exp'>
+      <h1>Explore The Experiences</h1>
+      <p className='explore-exp-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt tempore esse, vitae nemo assumenda et mollitia tenetur vero fugiat explicabo ipsum repudiandae rem .</p>
+      <div className="explore-exp-list">
+        {exp_list.map((item,index)=>{
+          return (
+            <div key={index} className="explore-exp-list-item">
+              <img src={item.exp_img} alt="" />
+              <p>{item.exp_img}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
