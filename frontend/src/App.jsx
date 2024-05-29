2e38d0997ca7392cfdd5492171569dc31bd4a627
Sowbaranika1111 <sowbika002@gmail.com>
2024-05-30 00:39:17 +0530

    [TU-1] Signup SignIn Popup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import User_profile from './pages/User_Profile/User_profile'
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+
 const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false)
+
   return (
     <>
+    {/* ternary operator */}{/*passing setShowLogin as props, true or false coming from LoginPopup component when the cross img is clicked it becomes false*/}
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/User_Profile' element={<User_profile />} />
