1903fa5ee66ebab0b4bcd9fad7793da3009b60dd
Sowbaranika1111 <sowbika002@gmail.com>
2024-05-30 17:17:04 +0530

    Completion of Front-end except AddYoursPopup.css file

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import {Link} from 'react-router-dom'
 
 const Navbar = ({setShowLogin}) => {
 
@@ -8,12 +9,12 @@ const Navbar = ({setShowLogin}) => {
 
   return (
     <div className='navbar'>
-      <img src={assets.boy_happy_img} alt="" className='logo '/>
+      <Link to='/'><img src={assets.boy_happy_img} alt="" className='logo '/></Link>
         <ul className="navbar-menu">
-          <li onClick = {() => setMenu("home")} className={menu==="home"?"active":""}>home</li>
-          <li onClick = {() => setMenu("experiences")} className={menu==="experiences"?"active":""}>experiences</li>
-          <li onClick = {() => setMenu("about us")} className={menu==="about us"?"active":""}>about us</li>
-          <li onClick = {() => setMenu("contact us")} className={menu==="contact us"?"active":""}>contact us</li>
+          <Link to='/' onClick = {() => setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+          <a href='#explore-exp' onClick = {() => setMenu("experiences")} className={menu==="experiences"?"active":""}>experiences</a>
+          <a href='#about-us' onClick = {() => setMenu("about us")} className={menu==="about us"?"active":""}>about us</a>
+          <a href='#footer' onClick = {() => setMenu("contact us")} className={menu==="contact us"?"active":""}>contact us</a>
         </ul>
         <div className="navbar-right">
           <div className="navbar-search-icon icon">
